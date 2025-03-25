67de94036f9a636d5e9064b32a32d1deaa8915a2
Matthew Girard <m.matt.girard@gmail.com>
2025-03-24 22:14:31 -0400

    add formatted output

--- src/app.py
+++ src/app.py
@@ -5,6 +5,12 @@ import nmr_parse
 with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
 
+
+def app_parse_nmr(nmr_string: str) -> [str, str]:
+    result = nmr_parse.parse_nmr(nmr_string)
+    return [result, result.replace('J', '_J_')]
+
+
 with gr.Blocks(
     title='NMR Parser'
 ) as APP:
@@ -20,25 +26,33 @@ with gr.Blocks(
                 info="NMR value string with journal precision",
                 show_copy_button=True
             )
+            nmr_output_markdown_label = gr.Markdown(
+                value="## Formatted Response"
+            )
+            nmr_output_markdown = gr.Markdown(
+                value="Formatted output will appear here",
+                label="Formatted Output",
+                show_copy_button=True
+            )
             with gr.Row():
                 nmr_submit = gr.Button(
                     value='Submit',
                     variant='primary'
                 )
                 nmr_clear = gr.ClearButton(
-                    components=[nmr_input, nmr_output]
+                    components=[nmr_input, nmr_output, nmr_output_markdown]
                 )
     gr.Examples(config['EXAMPLES'], inputs=[nmr_input])
     nmr_submit.click(
-        fn=nmr_parse.parse_nmr,
+        fn=app_parse_nmr,
         inputs=[nmr_input],
-        outputs=[nmr_output],
+        outputs=[nmr_output, nmr_output_markdown],
         api_name='nmr_parse'
     )
     nmr_input.submit(
-        fn=nmr_parse.parse_nmr,
+        fn=app_parse_nmr,
         inputs=[nmr_input],
-        outputs=[nmr_output],
+        outputs=[nmr_output, nmr_output_markdown],
         show_api=False
     )
 
